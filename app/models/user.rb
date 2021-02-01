ceea9f6a3fe7d3938ef07e75f9099eb8d75bbbff
hikaruuuuuu <hikkayn1202@gmail.com>
2021-02-01 17:18:09 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{7,128}\z/, message: "は、7 ~ 128文字以内の半角英数混合で入力してください" }
+  end
 end
