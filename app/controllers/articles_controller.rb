55983d7c4577a1c1cbda5d82c38ddf2c7cd301d4
hikaruuuuuu <hikkayn1202@gmail.com>
2021-02-09 20:52:25 +0900

    set create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,19 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text, :price, :hoge)#有料設定(buy_setting)のとこ今はhogeのまま
+  end
+
 end
