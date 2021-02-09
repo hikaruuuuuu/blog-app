9dcfa2e4cca8851bec08832258c1eeae3be10272
hikaruuuuuu <hikkayn1202@gmail.com>
2021-02-09 20:57:59 +0900

    set validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,14 @@
 class Article < ApplicationRecord
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :text
+    validates :buy_setting
+  end
+
+  validates :price, numericality: { only_integer: true, with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+  validates_inclusion_of :price, in: 100..9_999_999, message: 'は¥100〜9,999,999に設定してください'
+
   belongs_to :user
 end
