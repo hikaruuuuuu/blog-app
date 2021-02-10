41d3e034f8e8ef49226439bd996351a46f791542
hikaruuuuuu <hikkayn1202@gmail.com>
2021-02-10 22:27:43 +0900

    有料のとき保存できる

--- app/javascript/new_article_modal.js
+++ app/javascript/new_article_modal.js
@@ -6,6 +6,7 @@ function ModalClick() {
   let js_form_modal_not_free = document.getElementById("js-not-free");
   let js_form_modal_free = document.getElementById("js-free");
 
+  //有料無料のモーダル表示と非表示の切り替えのとこ
   js_form_posting_article.addEventListener("click", function(e) {
     e.preventDefault();
     if( js_form_modal.getAttribute("class") == "register-form-modal display_none") {
@@ -20,12 +21,14 @@ function ModalClick() {
     }
   });
 
+  //有料を選んだら、価格入力フォームのclassからhiddenしているdsplay_noneのclass名を外して表示させる
   js_form_modal_not_free.addEventListener("change", function() {
     if (js_form_modal_free.getAttribute("class") == "register-form-group__checkBox register-form-group__checkBox--free") {
       js_modal_price_box.classList.remove("display_none");
     }
   });
 
+  //無料を選んだら、価格入力フォームのclassからhiddenしているdsplay_noneのclass名を追加して消す
   js_form_modal_free.addEventListener("change", function() {
     if (js_form_modal_not_free.getAttribute("class") == "register-form-group__checkBox register-form-group__checkBox--notFree") {
       js_modal_price_box.classList.add("display_none")
