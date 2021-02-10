9131db41b596d1a2dbcbfa47a854042df474154d
hikaruuuuuu <hikkayn1202@gmail.com>
2021-02-10 23:51:48 +0900

    投稿機能できた

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,8 +9,14 @@ class Article < ApplicationRecord
   validates :buy_setting, inclusion: {in: [true, false]}
   # validates :buy_setting  boolean型にしているため、presence: trueだと、falseのときにエラーになる
 
-  validates :price, numericality: { only_integer: true, with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
-  validates_inclusion_of :price, in: 100..9_999_999, message: 'は¥100〜9,999,999に設定してください'
+  with_options if: :not_free do
+    validates :price, numericality: { only_integer: true, with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+    validates_inclusion_of :price, in: 100..9_999_999, message: 'は¥100〜9,999,999に設定してください'
+  end
+
+  def not_free
+    buy_setting == "true"
+  end
 
   belongs_to :user
   has_one_attached :image
