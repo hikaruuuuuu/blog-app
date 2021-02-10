e022a3e30bacd02f55799a41f326fbfeb082b731
hikaruuuuuu <hikkayn1202@gmail.com>
2021-02-11 00:02:58 +0900

    set 投稿のテスト
    投稿のテスト

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :article do
-    
+    title         {Faker::Lorem.word}
+    text          {Faker::Lorem.paragraph}
+    buy_setting   {Faker::Boolean.boolean}
+    price         {300}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/test.jpeg'), filename: 'test_image.png')
+    end
+
   end
 end
