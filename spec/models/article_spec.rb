b6b6a0f4b85e580132b3aea65da449ff89cd9121
hikaruuuuuu <hikkayn1202@gmail.com>
2021-02-11 00:05:15 +0900

    Update article_spec.rb

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @article = FactoryBot.build(:article)  
+    end
+
+    context '投稿できる' do
+    
+    end
+
+    context '投稿できない' do
+      
+    end
+
+  end
 end
