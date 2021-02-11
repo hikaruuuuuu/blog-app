d757dcab7a7e705602d26528b9f4de2a036b0a7b
hikaruuuuuu <hikkayn1202@gmail.com>
2021-02-11 18:12:05 +0900

    投稿できる

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,10 +7,44 @@ RSpec.describe Article, type: :model do
     end
 
     context '投稿できる' do
-    
+        it '全て正しく入力したら投稿できる' do
+          expect(@article).to be_valid
+        end
+        it '無料(false)の時はpriceなしで投稿できる' do
+          @article.buy_setting = false
+          @article.price = nil
+          expect(@article).to be_valid
+        end
     end
 
     context '投稿できない' do
+        it '画像がない' do
+          
+        end
+        it 'titleがない' do
+          
+        end
+        it 'textがない' do
+          
+        end
+        it 'buy_settingがない' do
+          
+        end
+        it 'buy_settingがtrueなのにpriceがない' do
+          
+        end
+        it 'priceが半角数次でない' do
+          
+        end
+        it 'priceが100円以下' do
+          
+        end
+        it 'priceが9,999,999万円以上' do
+          
+        end
+        it 'userがひもづいていない' do
+          
+        end
       
     end
 
