8a52b8a4386c2e326de5f5823c4843ec561c4690
hikaruuuuuu <hikkayn1202@gmail.com>
2021-03-14 15:12:08 +0900

    住所保存用のコントローラーの作成

--- app/controllers/user_deals_controller.rb
+++ app/controllers/user_deals_controller.rb
@@ -1,7 +1,20 @@
 class UserDealsController < ApplicationController
   def index
+    @address = Address.new
   end
 
   def create
+    @address = Address.new(address_params)
+    if @address.create
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def address_params
+    params.permit.(:f_name, :l_name, :f_name_kana, :l_name_kana, :postal_code, :prefecture_id, :city, :building_number, :building_name, :phone_number)
   end
 end
