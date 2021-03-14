152edb8115abc508516270c1006b270b28225081
hikaruuuuuu <hikkayn1202@gmail.com>
2021-03-14 16:55:55 +0900

    form_withに書き換え

--- app/controllers/user_deals_controller.rb
+++ app/controllers/user_deals_controller.rb
@@ -1,20 +1,19 @@
 class UserDealsController < ApplicationController
   def index
-    @address = Address.new
   end
 
   def create
-    @address = Address.new(address_params)
-    if @address.create
-      redirect_to root_path
-    else
-      render :index
-    end
+    user_deal = UserDeal.create(user_deal_params)
+    Address.create(address_params(user_deal))
   end
 
   private
 
-  def address_params
-    params.permit.(:f_name, :l_name, :f_name_kana, :l_name_kana, :postal_code, :prefecture_id, :city, :building_number, :building_name, :phone_number)
+  def user_deal_params
+    params.permit(:article_id).merge(user_id: current_user.id)
+  end
+
+  def address_params(user_deal)
+    params.permit(:f_name, :l_name, :f_name_kana, :l_name_kana, :postal_code, :prefecture_id, :city, :building_number, :building_name, :phone_number).merge(user_deal_id: user_deal.id)
   end
 end
