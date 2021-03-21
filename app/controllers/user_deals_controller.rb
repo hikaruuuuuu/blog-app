b4f8952bbea4e852784c79bb665097f09c4a0923
hikaruuuuuu <hikkayn1202@gmail.com>
2021-03-21 16:28:00 +0900

    Update user_deals_controller.rb

--- app/controllers/user_deals_controller.rb
+++ app/controllers/user_deals_controller.rb
@@ -5,6 +5,7 @@ class UserDealsController < ApplicationController
   end
 
   def create
+    binding.pry
     @article = Article.find(params[:article_id])
     @order = Order.new(order_params)
     if @order.valid?
@@ -18,7 +19,7 @@ class UserDealsController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:f_name, :l_name, :f_name_kana, :l_name_kana, :postal_code, :prefecture_id, :city, :building_number, :building_name, :phone_number).merge(user_id: current_user.id, article_id: params[:article_id])
+    params.require(:order).permit(:f_name, :l_name, :f_name_kana, :l_name_kana, :postal_code, :prefecture_id, :city, :building_number, :building_name, :phone_number).merge(user_id: current_user.id, article_id: params[:article_id]. token: params[:token])
   end
 
 end
